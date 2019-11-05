a19a861990111d56b8165d6faa120bf1d704819c
teddy_perissinotto <teddy.perissinotto@lapiscine.pro>
2019-11-05 18:29:33 +0100

    exoPhpPoo

---
+++ libraries/utils.php
@@ -0,0 +1,16 @@
+<?php
+
+function render(string $path, array $variables = []){
+    extract($variables);
+    //extract — Va me permettre de sortir les données de mon tableau sous forme de variable
+    ob_start();
+    require('templates/'.$path.'.html.php');
+    $pageContent = ob_get_clean();
+
+    require('templates/layout.html.php');
+}
+
+function redirect(string $url){
+    header("Location: $url");
+    exit();
+}
